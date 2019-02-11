9cc129ca448f02c422bbfe650d66d693b3560b74
IvanTurashov <cat-da-vinci@yandex.ru>
2019-02-11 20:03:57 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+/**
+ * Created by turashov on 14.08.2018.
+ */
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlTemplatePlugin = require('html-webpack-template');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        app: ['@babel/polyfill', './src/client/index.jsx']
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, './client/static'),
+        publicPath: '/'
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                loader: 'file-loader'
+            }
+        ]
+    },
+    devServer: {
+        port: 5011,
+        historyApiFallback: true,
+        open: true,
+        hot: true,
+        proxy: {
+            '/api': 'http://localhost:5010'
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Aviasales | Поиск',
+            filename: 'index.html',
+            template: HtmlTemplatePlugin,
+            inject: false,
+            mobile: true,
+            appMountId: 'app'
+        }),
+    ]
+};
