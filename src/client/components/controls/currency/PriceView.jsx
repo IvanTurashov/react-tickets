a01bd4e4a8e7ff1527102a692f44c74e644a127e
Turashov Ivan <turashov@element-soft.com>
2019-02-21 12:18:38 +0300

    Task-1 Format price

--- src/client/components/controls/currency/PriceView.jsx
+++ src/client/components/controls/currency/PriceView.jsx
@@ -1,14 +1,21 @@
-import React, { useMemo } from 'react';
+import React, { useMemo }  from 'react';
 import { connect } from 'react-redux';
 import { symbols } from "../../../constants/currencies.js";
 import { Money } from "../../../styles/ticket.js";
 
+function format(price) {
+    return price.toFixed(0).replace(/./g, function(match, idx, string) {
+        return idx > 0 && (string.length - idx) % 3 === 0 ? " " + match : match;
+    });
+}
+
 function PriceView({ currency, rates, price }) {
-    const converted = useMemo(() => {
-        return rates ? (price * rates[currency]).toFixed(0) : price;
+    const changedPrice = useMemo(() => {
+        const converted = rates ? (price * rates[currency]) : price;
+        return format(converted);
     }, [ currency, rates, price ]);
 
-    return <Money>{converted}&nbsp;{symbols[currency]}</Money>
+    return <Money>{changedPrice}&nbsp;{symbols[currency]}</Money>
 }
 
 function mapStateToProps(state) {
